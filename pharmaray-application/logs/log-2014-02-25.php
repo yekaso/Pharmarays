d0b98cb9c00039ff2b19bc1feb8786c66258a5b2
Onwuzu onyekachi <princeyekaso@gmail.com>
2014-02-25 16:33:12 +0100

    new log files

---
+++ pharmaray-application/logs/log-2014-02-25.php
@@ -0,0 +1,291 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2014-02-25 16:09:03 --> Config Class Initialized
+DEBUG - 2014-02-25 16:09:03 --> Hooks Class Initialized
+DEBUG - 2014-02-25 16:09:03 --> Utf8 Class Initialized
+DEBUG - 2014-02-25 16:09:03 --> UTF-8 Support Enabled
+DEBUG - 2014-02-25 16:09:03 --> URI Class Initialized
+DEBUG - 2014-02-25 16:09:03 --> Router Class Initialized
+DEBUG - 2014-02-25 16:09:03 --> No URI present. Default controller set.
+DEBUG - 2014-02-25 16:09:03 --> Output Class Initialized
+DEBUG - 2014-02-25 16:09:03 --> Security Class Initialized
+DEBUG - 2014-02-25 16:09:03 --> Input Class Initialized
+DEBUG - 2014-02-25 16:09:03 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-02-25 16:09:03 --> Language Class Initialized
+DEBUG - 2014-02-25 16:09:03 --> Loader Class Initialized
+DEBUG - 2014-02-25 16:09:03 --> Helper loaded: form_helper
+DEBUG - 2014-02-25 16:09:03 --> Helper loaded: url_helper
+DEBUG - 2014-02-25 16:09:03 --> Helper loaded: html_helper
+DEBUG - 2014-02-25 16:09:03 --> Database Driver Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Session Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Helper loaded: string_helper
+DEBUG - 2014-02-25 16:09:05 --> A session cookie was not found.
+DEBUG - 2014-02-25 16:09:05 --> Session routines successfully run
+DEBUG - 2014-02-25 16:09:05 --> Form Validation Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Controller Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> File loaded: ../pharmaray-application/views/index.php
+DEBUG - 2014-02-25 16:09:05 --> Final output sent to browser
+DEBUG - 2014-02-25 16:09:05 --> Total execution time: 1.2251
+DEBUG - 2014-02-25 16:09:05 --> Config Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Hooks Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Utf8 Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> UTF-8 Support Enabled
+DEBUG - 2014-02-25 16:09:05 --> URI Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Router Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Output Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Security Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Input Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-02-25 16:09:05 --> Language Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Loader Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Helper loaded: form_helper
+DEBUG - 2014-02-25 16:09:05 --> Helper loaded: url_helper
+DEBUG - 2014-02-25 16:09:05 --> Helper loaded: html_helper
+DEBUG - 2014-02-25 16:09:05 --> Database Driver Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Session Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Helper loaded: string_helper
+DEBUG - 2014-02-25 16:09:05 --> Session routines successfully run
+DEBUG - 2014-02-25 16:09:05 --> Form Validation Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Controller Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Model Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Model Class Initialized
+DEBUG - 2014-02-25 16:09:05 --> Session class already loaded. Second attempt ignored.
+INFO  - 2014-02-25 16:09:05 --> after session initialization................
+INFO  - 2014-02-25 16:09:05 --> after rays has been clicked.................
+INFO  - 2014-02-25 16:09:05 --> last article id is 0
+INFO  - 2014-02-25 16:09:05 --> The session id is ========>1a777c88705e499fede8ab3e9e48bea1 and the member id is =======>
+INFO  - 2014-02-25 16:09:05 --> The session id is ========>1a777c88705e499fede8ab3e9e48bea1 and the member id is =======>
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>>>>>
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>> AFTER >>>#22
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>>>>>#22
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>> AFTER >>>#21
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 1 the guest only data>>>>>#21
+INFO  - 2014-02-25 16:09:05 --> 1 the guest only data>> AFTER >>>#21
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>>>>>#21
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>> AFTER >>>#19
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>>>>>#19
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>> AFTER >>>#18
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>>>>>#18
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>> AFTER >>>#17
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>>>>>#17
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>> AFTER >>>#16
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 1 the guest only data>>>>>#16
+INFO  - 2014-02-25 16:09:05 --> 1 the guest only data>> AFTER >>>#16
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>>>>>#16
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>> AFTER >>>#14
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 1 the guest only data>>>>>#14
+INFO  - 2014-02-25 16:09:05 --> 1 the guest only data>> AFTER >>>#14
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>>>>>#14
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>> AFTER >>>#12
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 1 the guest only data>>>>>#12
+INFO  - 2014-02-25 16:09:05 --> 1 the guest only data>> AFTER >>>#12
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>>>>>#12
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>> AFTER >>>#10
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>>>>>#10
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>> AFTER >>>#9
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 1 the guest only data>>>>>#9
+INFO  - 2014-02-25 16:09:05 --> 1 the guest only data>> AFTER >>>#9
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>>>>>#9
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>> AFTER >>>#7
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 1 the guest only data>>>>>#7
+INFO  - 2014-02-25 16:09:05 --> 1 the guest only data>> AFTER >>>#7
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>>>>>#7
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>> AFTER >>>#5
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>>>>>#5
+INFO  - 2014-02-25 16:09:05 --> 0 the guest only data>> AFTER >>>#4
+INFO  - 2014-02-25 16:09:05 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:09:05 --> 1 the guest only data>>>>>#4
+INFO  - 2014-02-25 16:09:05 --> 1 the guest only data>> AFTER >>>#4
+DEBUG - 2014-02-25 16:09:05 --> File loaded: ../pharmaray-application/views/homepage.php
+DEBUG - 2014-02-25 16:09:05 --> Final output sent to browser
+DEBUG - 2014-02-25 16:09:05 --> Total execution time: 0.2350
+DEBUG - 2014-02-25 16:09:08 --> Config Class Initialized
+DEBUG - 2014-02-25 16:09:08 --> Hooks Class Initialized
+DEBUG - 2014-02-25 16:09:08 --> Utf8 Class Initialized
+DEBUG - 2014-02-25 16:09:08 --> UTF-8 Support Enabled
+DEBUG - 2014-02-25 16:09:08 --> URI Class Initialized
+DEBUG - 2014-02-25 16:09:08 --> Router Class Initialized
+DEBUG - 2014-02-25 16:09:08 --> Output Class Initialized
+DEBUG - 2014-02-25 16:09:08 --> Security Class Initialized
+DEBUG - 2014-02-25 16:09:08 --> Input Class Initialized
+DEBUG - 2014-02-25 16:09:08 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-02-25 16:09:08 --> Language Class Initialized
+DEBUG - 2014-02-25 16:09:08 --> Loader Class Initialized
+DEBUG - 2014-02-25 16:09:08 --> Helper loaded: form_helper
+DEBUG - 2014-02-25 16:09:08 --> Helper loaded: url_helper
+DEBUG - 2014-02-25 16:09:08 --> Helper loaded: html_helper
+DEBUG - 2014-02-25 16:09:08 --> Database Driver Class Initialized
+DEBUG - 2014-02-25 16:09:09 --> Session Class Initialized
+DEBUG - 2014-02-25 16:09:09 --> Helper loaded: string_helper
+DEBUG - 2014-02-25 16:09:09 --> Session routines successfully run
+DEBUG - 2014-02-25 16:09:09 --> Form Validation Class Initialized
+DEBUG - 2014-02-25 16:09:09 --> Controller Class Initialized
+DEBUG - 2014-02-25 16:09:09 --> Model Class Initialized
+DEBUG - 2014-02-25 16:09:09 --> Model Class Initialized
+DEBUG - 2014-02-25 16:09:09 --> Session class already loaded. Second attempt ignored.
+INFO  - 2014-02-25 16:09:09 --> after session initialization................
+INFO  - 2014-02-25 16:09:09 --> verify user roles ::::::::::===>SELECT `ur`.*
+FROM (`userrole` ur)
+JOIN `logindetails_userrole` ldur ON `ldur`.`logindetailsuserrole_userroleid` = `ur`.`id_userrole`
+JOIN `logindetails` l ON `l`.`id_logindetails` = `ldur`.`logindetailsuserrole_logindetailsid`
+WHERE lower(ur.name) =  'admin'
+AND `l`.`memberid_member` =  ''
+INFO  - 2014-02-25 16:09:09 --> inside retrieving comments member id and drug id is 1
+INFO  - 2014-02-25 16:09:09 --> SELECT `dc`.`id_drugcomment`, `dc`.`comment`, `dc`.`time_created`, `m`.`firstname_member`, `m`.`surname_member`, `mt`.`default_image_name`
+FROM (`drugcomment` dc)
+JOIN `member` m ON `dc`.`memberid_member` = `m`.`id_member` and dc.drugid_drug = 1
+JOIN `membertype` mt ON `m`.`membertypeid_membertype` = `mt`.`id_membertype`
+ORDER BY `dc`.`id_drugcomment` desc
+LIMIT 10
+INFO  - 2014-02-25 16:09:09 --> inside retrieving vacancy for member
+INFO  - 2014-02-25 16:09:09 --> Vacancies ::::::::::===>SELECT `vg`.`name` as vacancygroup_name, count(v.vacancy_id) as vacancy_count
+FROM (`vacancygroup` vg)
+JOIN `vacancy` v ON `vg`.`vacancygroup_id` = `v`.`VacancyGroupvacancygroup_id`
+GROUP BY `vg`.`vacancygroup_id`
+ORDER BY `vg`.`name`
+INFO  - 2014-02-25 16:09:09 --> inside retrieving comments member id and drug id is 1
+INFO  - 2014-02-25 16:09:09 --> SELECT `mt`.`id_membertype` as member_type_id, `mt`.`name` as membertype_name, count(dc.comment) as comment_count
+FROM (`drugcomment` dc)
+JOIN `member` m ON `dc`.`memberid_member` = `m`.`id_member` and dc.drugid_drug = 1
+JOIN `membertype` mt ON `m`.`membertypeid_membertype` = `mt`.`id_membertype`
+GROUP BY `mt`.`id_membertype`
+ORDER BY `dc`.`id_drugcomment`
+DEBUG - 2014-02-25 16:09:09 --> DB Transaction Failure
+ERROR - 2014-02-25 16:09:09 --> Query error: Table 'pharmarays.drugcategory_drug' doesn't exist
+DEBUG - 2014-02-25 16:09:09 --> Language file loaded: language/english/db_lang.php
+DEBUG - 2014-02-25 16:14:45 --> Config Class Initialized
+DEBUG - 2014-02-25 16:14:45 --> Hooks Class Initialized
+DEBUG - 2014-02-25 16:14:45 --> Utf8 Class Initialized
+DEBUG - 2014-02-25 16:14:45 --> UTF-8 Support Enabled
+DEBUG - 2014-02-25 16:14:45 --> URI Class Initialized
+DEBUG - 2014-02-25 16:14:45 --> Router Class Initialized
+DEBUG - 2014-02-25 16:14:45 --> No URI present. Default controller set.
+DEBUG - 2014-02-25 16:14:45 --> Output Class Initialized
+DEBUG - 2014-02-25 16:14:45 --> Security Class Initialized
+DEBUG - 2014-02-25 16:14:45 --> Input Class Initialized
+DEBUG - 2014-02-25 16:14:45 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-02-25 16:14:45 --> Language Class Initialized
+DEBUG - 2014-02-25 16:14:45 --> Loader Class Initialized
+DEBUG - 2014-02-25 16:14:45 --> Helper loaded: form_helper
+DEBUG - 2014-02-25 16:14:45 --> Helper loaded: url_helper
+DEBUG - 2014-02-25 16:14:45 --> Helper loaded: html_helper
+DEBUG - 2014-02-25 16:14:46 --> Database Driver Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Session Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Helper loaded: string_helper
+DEBUG - 2014-02-25 16:14:47 --> Session routines successfully run
+DEBUG - 2014-02-25 16:14:47 --> Form Validation Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Controller Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> File loaded: ../pharmaray-application/views/index.php
+DEBUG - 2014-02-25 16:14:47 --> Final output sent to browser
+DEBUG - 2014-02-25 16:14:47 --> Total execution time: 2.2671
+DEBUG - 2014-02-25 16:14:47 --> Config Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Hooks Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Utf8 Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> UTF-8 Support Enabled
+DEBUG - 2014-02-25 16:14:47 --> URI Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Router Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Output Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Security Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Input Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-02-25 16:14:47 --> Language Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Loader Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Helper loaded: form_helper
+DEBUG - 2014-02-25 16:14:47 --> Helper loaded: url_helper
+DEBUG - 2014-02-25 16:14:47 --> Helper loaded: html_helper
+DEBUG - 2014-02-25 16:14:47 --> Database Driver Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Session Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Helper loaded: string_helper
+DEBUG - 2014-02-25 16:14:47 --> Session routines successfully run
+DEBUG - 2014-02-25 16:14:47 --> Form Validation Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Controller Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Model Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Model Class Initialized
+DEBUG - 2014-02-25 16:14:47 --> Session class already loaded. Second attempt ignored.
+INFO  - 2014-02-25 16:14:47 --> after session initialization................
+INFO  - 2014-02-25 16:14:47 --> after rays has been clicked.................
+INFO  - 2014-02-25 16:14:47 --> last article id is 0
+INFO  - 2014-02-25 16:14:47 --> The session id is ========>5db6b24dc88edcc075f90526ac8299ac and the member id is =======>
+INFO  - 2014-02-25 16:14:47 --> The session id is ========>5db6b24dc88edcc075f90526ac8299ac and the member id is =======>
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>>>>>
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>> AFTER >>>#22
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>>>>>#22
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>> AFTER >>>#21
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 1 the guest only data>>>>>#21
+INFO  - 2014-02-25 16:14:47 --> 1 the guest only data>> AFTER >>>#21
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>>>>>#21
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>> AFTER >>>#19
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>>>>>#19
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>> AFTER >>>#18
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>>>>>#18
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>> AFTER >>>#17
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>>>>>#17
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>> AFTER >>>#16
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 1 the guest only data>>>>>#16
+INFO  - 2014-02-25 16:14:47 --> 1 the guest only data>> AFTER >>>#16
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>>>>>#16
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>> AFTER >>>#14
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 1 the guest only data>>>>>#14
+INFO  - 2014-02-25 16:14:47 --> 1 the guest only data>> AFTER >>>#14
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>>>>>#14
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>> AFTER >>>#12
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 1 the guest only data>>>>>#12
+INFO  - 2014-02-25 16:14:47 --> 1 the guest only data>> AFTER >>>#12
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>>>>>#12
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>> AFTER >>>#10
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>>>>>#10
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>> AFTER >>>#9
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 1 the guest only data>>>>>#9
+INFO  - 2014-02-25 16:14:47 --> 1 the guest only data>> AFTER >>>#9
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>>>>>#9
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>> AFTER >>>#7
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 1 the guest only data>>>>>#7
+INFO  - 2014-02-25 16:14:47 --> 1 the guest only data>> AFTER >>>#7
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>>>>>#7
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>> AFTER >>>#5
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>>>>>#5
+INFO  - 2014-02-25 16:14:47 --> 0 the guest only data>> AFTER >>>#4
+INFO  - 2014-02-25 16:14:47 --> The key doesnt exist <<<<<<<<<<<<<<>>>>>>>>>>>>>
+INFO  - 2014-02-25 16:14:47 --> 1 the guest only data>>>>>#4
+INFO  - 2014-02-25 16:14:47 --> 1 the guest only data>> AFTER >>>#4
+DEBUG - 2014-02-25 16:14:47 --> File loaded: ../pharmaray-application/views/homepage.php
+DEBUG - 2014-02-25 16:14:47 --> Final output sent to browser
+DEBUG - 2014-02-25 16:14:47 --> Total execution time: 0.5190
