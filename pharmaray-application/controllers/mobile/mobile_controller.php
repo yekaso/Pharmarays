56429eef22015cfa14f05dec1795c30273c4f998
yekaso <onyekachionwuzu@Kachi.local>
2014-03-15 13:54:53 +0100

    After completing the implementation of pharmacy drugs...

--- pharmaray-application/controllers/mobile/mobile_controller.php
+++ pharmaray-application/controllers/mobile/mobile_controller.php
@@ -43,11 +43,11 @@ class mobile_controller extends CI_Controller {
                 'logged_in_user' => $data['logged_in_user'],
                 'membertypeid' => $data['membertypeid']
             );
+            $data['pharmacydata'] = $this->mobile_model->retrieve_pharmacy($member_id);
 
             $data['is_adminuser'] = $this->mobile_model->verify_user_role($data['memberid']);
-            $data = array(
-                'status' => 'success',
-                'message' => 'User Exists');
+            $data['status'] = 'success';
+            $data['message'] = 'User Exists';
         } else {
             $data = array(
                 'status' => 'error',
@@ -60,7 +60,7 @@ class mobile_controller extends CI_Controller {
         extract($_POST);
         $data = '';
         $gender_value = '0';
-        if($gender === 'Female'){
+        if ($gender === 'Female') {
             $gender_value = '1';
         }
         $member_type_id = $this->mobile_model->retrieve_membertype_byname($member_type);
@@ -72,7 +72,7 @@ class mobile_controller extends CI_Controller {
             'emailaddress_member' => $email,
             'membertypeid_membertype' => $member_type_id
         );
-        
+
         $data['membertypeimage'] = 'profilem';
         $data['membertypeid'] = $member_type_id;
         $logindetails_data = array(
@@ -91,22 +91,128 @@ class mobile_controller extends CI_Controller {
             if ($member_id > 0) {
                 $logindetails_data['memberid_member'] = $member_id;
                 $this->membermodel->create_new_logindetails($logindetails_data);
-                $data = array(
-                    'status' => 'success',
-                    'message' => 'Registration complete');
+                $data['status'] = 'success';
+                $data['message'] = 'Registration complete';
                 $data['memberid'] = $member_id;
             } else {
                 log_message('info', 'the registration was incomplete : invalid registration information.................');
                 $data = array(
                     'status' => 'error',
                     'message' => 'Invalid registration information');
-             }
+            }
         } else {
             $data = array(
                 'status' => 'error',
                 'message' => 'A user exists already with this email address');
         }
         echo json_encode(array($data));
+    }
+
+    function retrieve_drugs() {
+        extract($_POST);
+        $data['drugs'] = $this->mobile_model->retrieve_drug($pharmacyid);
+
+        $counter = 0;
+
+        if ($data['drugs']) {
+            foreach ($data['drugs'] as $result_data) {
+                $drug_company = $result_data['drug_company'];
+                $split_companyname = explode(",", $drug_company);
+                $split_companyname = array_unique($split_companyname);
+                $company_list = implode(',', $split_companyname);
+                $data['drugs'][$counter]['drug_company'] = $company_list;
+
+                $counter++;
+            }
+        }
+
+        $pharmacy_info = $this->mobile_model->retrieve_pharmacy($member_id);
+        $data['pharmacy'] = $this->word_trim($pharmacy_info->name, 120, true);
+        $data['status'] = 'success';
+        $data['message'] = 'Retrieve Drugs...' . $company_list;
+        //  $data['available_drugs'] = $this->mobile_model->retrievedrugsbypharmacy($pharmacy_info->id);
+        echo json_encode(array($data));
+    }
+
+    function word_trim($string, $count, $ellipsis = FALSE) {
+
+        if (strlen($string) > $count) {
+            $string = substr($string, 0, strrpos(substr($string, 0, $count), ' '));
+            if (is_string($ellipsis)) {
+                $string .=$ellipsis;
+            } else if ($ellipsis) {
+                $string .= '&hellip;';
+            }
+        }
+        return $string;
+    }
+
+    function deletedrugavailability() {
+        extract($_POST);
+        $drugavailability = $this->mobile_model->delete_drugavailability($pharmacyid, $drugid_data);
+        log_message('info', 'After creating drug availability');
+        $data = array(
+            'status' => 'success',
+            'message' => 'Drug relation deleted');
+        echo json_encode(array($data));
+    }
+
+    function processdrugavailability() {
+        extract($_POST);
+        $createdata_array = explode(",", $createdata);
+        $errors1 = array_filter($createdata_array);
+        if (!empty($errors1)) {
+            $drugavailability_create_data = array();
+            foreach ($createdata_array as $drugid) {
+                $split_data = array(
+                    'pharmacyid_pharmacy' => $pharmacyid,
+                    'drugid_drug' => $drugid,
+                );
+                $drugavailability_create_data[] = $split_data;
+            }
+            $errors = array_filter($drugavailability_create_data);
+            if (!empty($errors)) {
+                $this->mobile_model->create_drugavailability($drugavailability_create_data);
+            }
+        }
+        $deletedata_array = explode(",", $deletedata);
+        $delerrors = array_filter($deletedata_array);
+        if (!empty($delerrors)) {
+            $this->mobile_model->delete_drugavailability($pharmacyid, $deletedata_array);
+        }
+        $data = array(
+            'status' => 'success',
+            'message' => 'Drug relation deleted');
+        echo json_encode(array($data));
+    }
+
+    function createdrugavailability() {
+        extract($_POST);
+        log_message('info', 'Inside create pharmacy');
+        $drugavailability_data = array(
+            'pharmacyid_pharmacy' => $pharmacyid,
+            'drugid_drug' => $drugid,
+        );
+        $drugavailability = $this->mobile_model->create_drugavailability($drugavailability_data);
+        log_message('info', 'After creating drug availability');
+        $data = array(
+            'status' => 'success',
+            'message' => 'Drug relation created');
+        echo json_encode(array($data));
+    }
+
+    function selectdrugsbypharmacy() {
+        extract($_POST);
+        $data = $this->mobile_model->retrievedrugsbypharmacy($pharmacyid);
+        echo json_encode($data);
+    }
+
+    function retrieve_pharmacy() {
+        extract($_POST);
+        $last_pharmacyid = 0;
+        $locationid = 0;
+        $data = $this->mobile_model->retrieve_pharmacy_bylimit($last_pharmacyid, $locationid,$limit);
+        echo json_encode($data);
     }
 
 }
--- pharmaray-application/models/mobile_model/mobile_model.php
+++ pharmaray-application/models/mobile_model/mobile_model.php
@@ -16,7 +16,8 @@ class mobile_model extends CI_Model {
         parent::__construct();
         $this->load->database();
     }
-   function verify_user_role($memberid) {
+
+    function verify_user_role($memberid) {
         $this->db->select('ur.*, ')
                 ->from('userrole ur')
                 ->join('logindetails_userrole ldur', 'ldur.logindetailsuserrole_userroleid = ur.id_userrole')
@@ -31,20 +32,184 @@ class mobile_model extends CI_Model {
         $query->free_result();
         return $result;
     }
-    function retrieve_membertype_byname($member_type_name){
+
+    function retrieve_membertype_byname($member_type_name) {
         $member_type = strtolower($member_type_name);
         $this->db->select('mt.id_membertype, ')
                 ->from('membertype mt')
-                  ->where(array(
+                ->where(array(
                     'lower(mt.name)' => "$member_type",
                         )
         );
-        
+
         $query = $this->db->get();
         $result = $query->row();
         log_message('info', 'verify user roles ::::::::::===>' . $this->db->last_query());
         $query->free_result();
         return $result->id_membertype;
     }
+
+    function retrieve_pharmacy($memberid) {
+        $pharm_rolename = 'pharmacy owner';
+
+        $this->db->select("p.name, p.id_pharmacy as id", false)
+                ->from('pharmacy p')
+                ->join('userrole_pharmacy urp', 'p.id_pharmacy = urp.pharmacy_id')
+                ->join('logindetails_userrole lur', 'lur.logindetailsuserrole_userroleid = urp.loginuserrole_id')
+                ->join('userrole ur', 'ur.id_userrole = urp.loginuserrole_id')
+                ->join('logindetails ld', 'ld.id_logindetails = lur.logindetailsuserrole_logindetailsid')
+                ->where(array(
+                    'lower(ur.name)' => "$pharm_rolename",
+                    'ld.memberid_member' => "$memberid"
+                ))
+                ->order_by('p.name');
+
+
+        $query = $this->db->get();
+        $result = $query->row();
+        log_message('info', 'verify user roles ::::::::::===>' . $this->db->last_query());
+        $query->free_result();
+        return $result;
+    }
+
+    function retrieve_drug($pharmacyid) {
+
+        $this->db->select("da.drugid_drug as id")
+                ->from('drugavailability da')
+                ->where(array(
+                    'da.pharmacyid_pharmacy' => "$pharmacyid"
+        ));
+        $this->db->distinct();
+        $query = $this->db->get();
+        $query1 = $this->db->last_query();
+
+
+
+        $wherequery1 = 'd.id_drug not in (' . $query1 . ')';
+        $wherequery2 = 'd.id_drug in (' . $query1 . ')';
+
+        $this->db->select('d.id_drug,d.drug_name,d.drug_description, group_concat(bn.name) as drug_company,group_concat(dcc.name) as drug_brandnames, dcc.id_drugcategory as category_id,"true" as notChecked', false)
+                ->from('drug d')
+                ->join('drugcategory_drug dcg', 'dcg.drug_id = d.id_drug')
+                ->join('drugcategory dcc', 'dcc.id_drugcategory = dcg.drugcategory_id')
+                ->join('brandname bn', 'bn.id_brandname = dcc.brandname_id')
+                ->where($wherequery1)
+                ->order_by('d.drug_name,bn.id_brandname');
+
+        $this->db->group_by("id_drug");
+        $query = $this->db->get();
+
+        $query2 = $this->db->last_query();
+
+
+        $this->db->select('d.id_drug,d.drug_name,d.drug_description, group_concat(bn.name, ":", bn.id_brandname) as drug_company,group_concat(dcc.name) as drug_brandnames, dcc.id_drugcategory as category_id,"false" as notChecked', false)
+                ->from('drug d')
+                ->join('drugcategory_drug dcg', 'dcg.drug_id = d.id_drug')
+                ->join('drugcategory dcc', 'dcc.id_drugcategory = dcg.drugcategory_id')
+                ->join('brandname bn', 'bn.id_brandname = dcc.brandname_id')
+                ->where($wherequery2)
+                ->order_by('d.drug_name,bn.id_brandname');
+
+        $this->db->group_by("id_drug");
+        $query = $this->db->get();
+
+        $query3 = $this->db->last_query();
+
+        $final_query = '(' . $query2 . ') union (' . $query3 . ')';
+        log_message('info', 'Ajax retrieval for articles ::::::::::===>' . $final_query);
+        $query = $this->db->query($final_query);
+        //   $this->db->get();
+
+        $result = $query->result_array();
+        foreach ($result as $key => $subarray) {
+            foreach ($subarray as $subkey => $subsubarray) {
+                if ($result[$key]['notChecked'] == 'true') {
+                    $result[$key]['notChecked'] = true;
+                } else if ($result[$key]['notChecked'] == 'false') {
+                    $result[$key]['notChecked'] = false;
+                }
+            }
+        }
+        //   log_message('info', 'new result ======================='.$result);
+        $query->free_result();
+        return $result;
+
+
+        /*  $this->db->select("d.id_drug,d.drug_name,d.drug_description, group_concat(bn.name, ':', bn.id_brandname) as drug_company,group_concat(dcc.name) as drug_brandnames, dcc.id_drugcategory as category_id", false)
+          ->from('drug d')
+          ->join('drugcategory_drug dcg', 'dcg.drug_id = d.id_drug')
+          ->join('drugcategory dcc', 'dcc.id_drugcategory = dcg.drugcategory_id')
+          ->join('brandname bn', 'bn.id_brandname = dcc.brandname_id')
+          ->order_by('d.drug_name,bn.id_brandname');
+
+          $this->db->group_by("id_drug");
+          $query = $this->db->get();
+          log_message('info', 'Drugs ::::::::::===>' . $this->db->last_query());
+          $result = $query->result_array();
+          $query->free_result();
+          return $result;
+         * 
+         */
+    }
+
+    function retrievedrugsbypharmacy($pharmacyid) {
+        $this->db->select("da.drugid_drug as id")
+                ->from('drugavailability da')
+                ->where(array(
+                    'da.pharmacyid_pharmacy' => "$pharmacyid"
+        ));
+        $this->db->distinct();
+        $query = $this->db->get();
+        log_message('info', 'Drugs ::::::::::===>' . $this->db->last_query());
+        $result = $query->result_array();
+        $query->free_result();
+        return $result;
+    }
+
+    function create_drugavailability($drugavailability_data) {
+        log_message('info', 'before inserting into drug availability.................');
+        $this->db->insert_batch('drugavailability', $drugavailability_data);
+        log_message('info', 'after inserting into drug availability.................');
+
+        log_message('info', $this->db->last_query());
+        return $this->db->insert_id();
+    }
+
+    function delete_drugavailability($pharmacyid, $drugid_data) {
+        log_message('info', 'before deleting from drug availability.................');
+        $wherequery = 'drugid_drug IN (' . implode(',', $drugid_data) . ') and pharmacyid_pharmacy = ' . $pharmacyid;
+
+        $this->db->where($wherequery, NULL, FALSE);
+        $this->db->delete('drugavailability');
+
+        log_message('info', $this->db->last_query());
+        return;
+    }
+
+    function retrieve_pharmacy_bylimit($pharmacyid, $locationid, $limit) {
+        log_message('info', 'last pharmacy id is ' . $pharmacyid);
+        if ($pharmacyid == 0) {
+            $where_sql = "p.id_pharmacy  > 0";
+        } else {
+            $where_sql = "p.id_pharmacy  > " . $pharmacyid;
+        }
+        if ($locationid > 0) {
+            $where_sql .= " and l.id_location = " . $locationid;
+        }
+        $this->db->select("p.id_pharmacy as id,p.name,p.telephone,p.address,l.name as location", false)
+                ->from('pharmacy p')
+                ->join('location l', 'p.locationid_location = l.id_location')
+                ->where($where_sql)
+                ->order_by('p.id_pharmacy,p.name', 'desc');
+        $this->db->limit($limit);
+        $query = $this->db->get();
+        log_message('info', 'Ajax retrieval ::::::::::===>' . $this->db->last_query());
+
+        $result = $query->result_array();
+        $query->free_result();
+        return $result;
+    }
+
 }
+
 ?>
